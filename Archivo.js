93b138f4724e9f71e73f82d85ee2480f609f744b
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-05-04 00:17:59 -0500

    pasaremos una función como prop desde Board a Square

--- Archivo.js
+++ Archivo.js
@@ -16,7 +16,9 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]} 
+    onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
