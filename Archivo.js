f174be2a49c88afb63cf2c6ae125d01719eefa31
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-05-03 23:41:15 -0500

    agregando constructor

--- Archivo.js
+++ Archivo.js
@@ -1,4 +1,10 @@
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
       <button className="square" onClick={() => console.log('click')}>
