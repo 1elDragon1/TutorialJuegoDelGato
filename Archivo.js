62ead62dd521731e51839f4498d4341cd3528a15
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-05-04 00:36:50 -0500

    modificacion de codigo

--- Archivo.js
+++ Archivo.js
@@ -1,19 +1,24 @@
 class Square extends React.Component {
-  
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
-}
 
-class Board extends React.Component {
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} 
-    onClick={() => this.handleClick(i)}
-    />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
