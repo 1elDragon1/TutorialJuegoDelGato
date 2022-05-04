bc7a737429f66125bfc23c783e5838e470bc4611
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-05-04 00:43:14 -0500

    acomodando codigo

--- Archivo.js
+++ Archivo.js
@@ -1,4 +1,17 @@
 class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
